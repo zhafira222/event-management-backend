9b0f1dd34bd3900d3b8bfd0f7372047e9eb9f4d3
Samuel B <sambjhr.work@gmail.com>
2026-01-09 22:02:30 +0800

    review list (#16)

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { ReviewService } from "./review.service";
 import { CreateReviewDTO } from "./dto/create-review.dto";
+import { ApiError } from "../../utils/api-error";
 
 export class ReviewController {
   reviewService: ReviewService;
@@ -20,10 +21,27 @@ export class ReviewController {
     return res.status(200).send(result);
   };
 
-  // OPTIONAL: untuk event detail page (public)
   getReviewsByEventId = async (req: Request, res: Response) => {
     const eventId = req.params.eventId;
     const result = await this.reviewService.getReviewsByEventId(eventId);
     return res.status(200).send({ message: "Get reviews success", data: result });
+  };
+
+  getReviewsByOrganizerId = async (req: Request, res: Response) => {
+    const organizerId = Number(req.params.organizerId);
+    if (!organizerId || Number.isNaN(organizerId)) {
+      throw new ApiError("organizerId must be a number", 400);
+    }
+
+    const take = req.query.take ? Number(req.query.take) : 6;
+    const result = await this.reviewService.getReviewsByOrganizerId(
+      organizerId,
+      take
+    );
+
+    return res.status(200).send({
+      message: "Get reviews by organizer success",
+      data: result,
+    });
   };
 }
